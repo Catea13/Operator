f3cac69050df6c3827d4d327ddbbfda079011c41
Catea13 <58842293+Catea13@users.noreply.github.com>
2020-11-18 16:08:43 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main extends Nr {
+
+
+    public Main(int a, int b) {
+        super(a, b);
+    }
+
+    public static void main(String[] args) {
+        Nr main=new Nr(5,7);
+        int d=main.a+main.b;
+        System.out.println(d);
+
+    }
+    }
+
+
---
+++ src/com/company/Nr.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Nr {
+    int a;
+    int b;
+
+
+    public Nr(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
